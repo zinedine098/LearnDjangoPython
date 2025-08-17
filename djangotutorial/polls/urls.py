7797e063ec1c37ec5b0fcee2747105d34f9351a5
zinedine098 <ibnuwawan80@gmail.com>
2025-08-17 21:47:42 +0700

    saya menambahkan navbar dan menambahkan link url di dalamnya

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # /polls/ akan diarahkan ke views.index    
+    path('', views.index, name='home'),  # /polls/ akan diarahkan ke views.index    
     path('products/', views.Products, name='products'),
     path('custumer/', views.Custumer, name='custumer'),
 ]
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -7,17 +7,22 @@ from django.http import HttpResponse
 def index(request):
     context = {
         'title': 'Halaman Index',
+        'menu' : 'home'
     }
     return render(request, 'data/main.html', context)
 
 def Products(request):
     context = {
         'title': 'Halaman Products',
+        'menu' : 'products'
+
     }
     return render(request, 'data/products.html', context)
 
 def Custumer(request):
     context = {
         'title': 'Halaman Custumer',
+        'menu' : 'custumer'
+
     }
     return render(request, 'data/custumer.html', context)
